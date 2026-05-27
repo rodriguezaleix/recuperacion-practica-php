eb6537d048e3149fbc7c465a6e3221ea8563d126
aleix rodriguez <aleix.rodriguez@gracia.lasalle.cat>
2026-05-27 20:11:19 +0200

    Implementar conexion PDO a base de datos

--- src/db.php
+++ src/db.php
@@ -2,4 +2,23 @@
 class Database {
     private static $instance = null;
     private $connection;
+
+    private function __construct() {
+        $host = 'localhost';
+        $db   = 'taquilla_parque';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8mb4';
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        try {
+            $this->connection = new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
 }

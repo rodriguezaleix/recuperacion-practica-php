9c7f6d59437039ded93009dbddd015b90cf27a4e
aleix rodriguez <aleix.rodriguez@gracia.lasalle.cat>
2026-05-27 20:11:19 +0200

    Estructura inicial HTML de la pagina principal

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require_once 'db.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Parque de Dinosaurios</title>
+</head>
+<body>
+</body>
+</html>
